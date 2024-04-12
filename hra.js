7dafe52f4317c63bf458ceeb083bdc77f23e7232
Nikol Karnikova <ni.karnikova@gmail.com>
2024-04-12 09:04:56 +0200

    Oprava- přidání id tlacitkum.

--- hra.js
+++ hra.js
@@ -3,9 +3,9 @@ let currentPlayer = 'circle'
 const selectbox = (event) => {
   event.target.disabled = true;
   if (currentPlayer === 'circle') {
+    document.querySelector('.actualplayer').src = 'images/circle.svg';
     currentPlayer = 'cross';
     event.target.classList.add('board__field--cross');
-    document.querySelector('.actualplayer').src = 'images/circle.svg';
   } else {
     currentPlayer = 'circle';
     event.target.classList.add('board__field--circle');
@@ -14,13 +14,14 @@ const selectbox = (event) => {
 };
 
 
-  document.querySelector('button:nth-child(1)').addEventListener('click', selectbox);
-  document.querySelector('button:nth-child(2)').addEventListener('click', selectbox);
-  document.querySelector('button:nth-child(3)').addEventListener('click', selectbox);
-  document.querySelector('button:nth-child(4)').addEventListener('click', selectbox);
-  document.querySelector('button:nth-child(5)').addEventListener('click', selectbox);
-  document.querySelector('button:nth-child(6)').addEventListener('click', selectbox);
-  document.querySelector('button:nth-child(7)').addEventListener('click', selectbox);
-  document.querySelector('button:nth-child(8)').addEventListener('click', selectbox);
-  document.querySelector('button:nth-child(9)').addEventListener('click', selectbox);
-  document.querySelector('button:nth-child(10)').addEventListener('click', selectbox);
+  document.querySelector('#box1').addEventListener('click', selectbox);
+  document.querySelector('#box2').addEventListener('click', selectbox);
+  document.querySelector('#box3').addEventListener('click', selectbox);
+  document.querySelector('#box4').addEventListener('click', selectbox);
+  document.querySelector('#box5').addEventListener('click', selectbox);
+  document.querySelector('#box6').addEventListener('click', selectbox);
+  document.querySelector('#box7').addEventListener('click', selectbox);
+  document.querySelector('#box8').addEventListener('click', selectbox);
+  document.querySelector('#box9').addEventListener('click', selectbox);
+  document.querySelector('#box10').addEventListener('click', selectbox);
+

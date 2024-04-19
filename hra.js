492be6ea61803fd442e160b2db67b14758f167f1
Nikol Karnikova <ni.karnikova@gmail.com>
2024-04-19 20:50:06 +0200

    pridani viteze

--- hra.js
+++ hra.js
@@ -1,27 +1,64 @@
-let currentPlayer = 'circle'  
+import { findWinner } from "https://unpkg.com/piskvorky@0.1.4";
+
+let currentPlayer = "circle";
 
 const selectbox = (event) => {
   event.target.disabled = true;
-  if (currentPlayer === 'circle') {
-    document.querySelector('.actualplayer').src = 'images/circle.svg';
-    currentPlayer = 'cross';
-    event.target.classList.add('board__field--cross');
+  if (currentPlayer === "circle") {
+    document.querySelector(".actualplayer").src = "images/circle.svg";
+    currentPlayer = "cross";
+    event.target.classList.add("board__field--cross");
   } else {
-    currentPlayer = 'circle';
-    event.target.classList.add('board__field--circle');
-    document.querySelector('.actualplayer').src = 'images/cross.svg';
+    currentPlayer = "circle";
+    event.target.classList.add("board__field--circle");
+    document.querySelector(".actualplayer").src = "images/cross.svg";
   }
+  result();
+  console.log(createArray());
 };
 
+const boxes = document.querySelectorAll(".box");
+boxes.forEach((button) => {
+  button.addEventListener("click", selectbox);
+});
 
-  document.querySelector('#box1').addEventListener('click', selectbox);
-  document.querySelector('#box2').addEventListener('click', selectbox);
-  document.querySelector('#box3').addEventListener('click', selectbox);
-  document.querySelector('#box4').addEventListener('click', selectbox);
-  document.querySelector('#box5').addEventListener('click', selectbox);
-  document.querySelector('#box6').addEventListener('click', selectbox);
-  document.querySelector('#box7').addEventListener('click', selectbox);
-  document.querySelector('#box8').addEventListener('click', selectbox);
-  document.querySelector('#box9').addEventListener('click', selectbox);
-  document.querySelector('#box10').addEventListener('click', selectbox);
+const createArray = () => {
+  let array = [];
+  boxes.forEach((button) => {
+    if (button.classList.contains("board__field--circle")) {
+      array.push("o");
+    } else if (button.classList.contains("board__field--cross")) {
+      array.push("x");
+    } else {
+      array.push("_");
+    }
+  });
+  return array;
+};
 
+const result = () => {
+  let array = createArray();
+
+  const winner = findWinner(array);
+
+  if (winner === "o" || winner === "x") {
+    setTimeout(() => {
+      alert(`Vítězem je hráč se symbolem ${winner}.`);
+    }, 1000);
+    setTimeout(() => {
+      location.reload();
+    }, 2000);
+    return false;
+  }
+
+  if (winner === "tie") {
+    setTimeout(() => {
+      alert(`Hra skončila remízou.`);
+    }, 1000);
+    setTimeout(() => {
+      location.reload();
+    }, 2000);
+    return false;
+  }
+  return true;
+};
